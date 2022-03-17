f52f737477792afae9a8978b37510a7199ab1e89
RaquelVR00 <r.vazquez13@uspceu.es>
2022-03-17 23:56:17 +0100

    Base de datos

--- src/pharmacyappfinalproject/PharmacyAppFinalProject.java
+++ src/pharmacyappfinalproject/PharmacyAppFinalProject.java
@@ -5,6 +5,11 @@
  */
 package pharmacyappfinalproject;
 
+import db.interfaces.ComorbidityManager;
+import db.interfaces.DBManager;
+import db.interfaces.PatientManager;
+import db.interfaces.TreatmentManager;
+import db.sqlite.SQLiteManager;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -18,6 +23,10 @@ import javafx.stage.Stage;
  * @author inesd
  */
 public class PharmacyAppFinalProject extends Application {
+    private static DBManager dbManager;
+    private static ComorbidityManager comorbidityManager;
+    private static TreatmentManager treatmentManager;
+    private static PatientManager patientManager;
     
     @Override
     public void start(Stage primaryStage) {
@@ -39,12 +48,41 @@ public class PharmacyAppFinalProject extends Application {
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
+        
+        
+
+        
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        dbManager = new SQLiteManager();
+        comorbidityManager = dbManager.getComorbidityManager();
+        treatmentManager = dbManager.getTreatmentManager();
+        patientManager = dbManager.getPatientManager();
+        
+        String string = null;
+        int patient_id=0;
+        ComorbidityManager comorbiditymanager;
+
+        String comorbidity[] = string.split(",");
+        for(int i=0; i<comorbidity.length;i++){
+        String symptom = comorbidity[i];
+        comorbidityManager.add(symptom);
+        int comorbidityId = dbManager.getLastId();
+        patientManager.assign_comorbidity(patient_id, comorbidityId);
+        }
+
+
+        String treatment[] = string.split(",");
+        for(int i=0; i<treatment.length;i++){
+        String symptom = treatment[i];
+        treatmentManager.add(symptom);
+        int treatmentId= dbManager.getLastId();
+        patientManager.assign_treatment(patient_id, treatmentId);
+        }
         launch(args);
     }
     
